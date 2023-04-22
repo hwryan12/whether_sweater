02318e1cfc1a359ea24aa6030a78a2f54635fbaa
Harrison Ryan <hwryan12@gmail.com>
2023-04-22 15:42:11 -0500

    feature: Add route and controller for login request/Completes endpoint

---
+++ app/controllers/api/v0/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V0::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      render json: { data: { type: 'users', id: user.id, attributes: { email: user.email, api_key: user.api_key } } }, status: :ok
+    else
+      render json: { error: 'Invalid credentials' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
---
+++ spec/requests/login_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v0/sessions', type: :request do
+  context 'when the request is valid' do
+    let!(:user) { create(:user, email: 'whatever@example.com', password: 'password') }
+    it 'returns the user with an api key and a 200 code' do
+      post '/api/v0/sessions', params: { email: user.email, password: user.password }, headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }, as: :json
+      
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json_response[:data][:id].to_i).to eq(user.id)
+      expect(json_response[:data][:attributes][:email]).to eq(user.email)
+      expect(json_response[:data][:attributes][:api_key]).to eq(user.api_key)
+    end
+  end
+end
