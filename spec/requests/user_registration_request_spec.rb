8ee5d9f4d2e65e5c64170bc7471f41e1237bea19
Harrison Ryan <hwryan12@gmail.com>
2023-04-22 15:20:26 -0500

    feature: Implement create action logic in users controller/Completes endpoint 2

---
+++ spec/requests/user_registration_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v0/users', type: :request do
+  context 'when the request is valid' do
+    let!(:user_params) do
+      {
+        user: {
+          email: 'email@email.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+
+    it 'creates a new user returns a 201 code' do
+      post '/api/v0/users', params: user_params, headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }, as: :json
+
+      expect(response).to have_http_status(:created)
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+  
+      expect(json_response).to be_a(Hash)
+      expect(json_response).to have_key(:data)
+      expect(json_response[:data]).to be_a(Hash)
+      expect(json_response[:data]).to have_key(:id)
+      expect(json_response[:data][:id]).to be_a(Integer)
+      expect(json_response[:data]).to have_key(:type)
+      expect(json_response[:data][:type]).to eq('users')
+      expect(json_response[:data]).to have_key(:attributes)
+      expect(json_response[:data][:attributes]).to be_a(Hash)
+      expect(json_response[:data][:attributes]).to have_key(:email)
+      expect(json_response[:data][:attributes][:email]).to be_a(String)
+      expect(json_response[:data][:attributes]).to have_key(:api_key)
+      expect(json_response[:data][:attributes][:api_key]).to be_a(String)
+    end
+  end
+end
