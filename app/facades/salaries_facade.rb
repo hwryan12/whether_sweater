90c114ca7ed5dbb8ccf694dedc58f93469775db2
Harrison Ryan <hwryan12@gmail.com>
2023-04-24 10:52:53 -0500

    feature: Create salaries facade

---
+++ app/facades/salaries_facade.rb
@@ -0,0 +1,16 @@
+class SalariesFacade
+  def get_tech_salaries(location)
+    city = remove_state_from_city(location)
+    salaries = TeleportService.new.get_city_salaries(city)
+    location_coords = GeocodeService.new.get_coordinates(location)
+    weather = WeatherApiService.new.get_forecast(location_coords[:lat], location_coords[:lng])
+
+    SalariesSerializer.new(salaries, weather).serialize_salaries
+  end
+  
+  private
+
+  def remove_state_from_city(city_with_state)
+    city_with_state.split(',').first.strip
+  end
+end
--- spec/final/salaries_facade_spec.rb
+++ spec/final/salaries_facade_spec.rb
@@ -3,12 +3,28 @@ require 'rails_helper'
 RSpec.describe SalariesFacade, type: :facade do
   context 'instance methods' do
     describe '#get_tech_salaries' do
-      let!(:city) { 'Denver, CO' }
+      let!(:city) { 'Houston, TX' }
       it 'returns a tech salaries for a given city as serialzed JSON' do
         VCR.use_cassette('salaries_facade') do
           salaries = SalariesFacade.new.get_tech_salaries(city)
           binding.pry
           expect(salaries).to be_a(Hash)
+          expect(salaries[:data]).to be_a(Hash)
+          expect(salaries[:data][:id]).to be_nil
+          expect(salaries[:data][:type]).to eq('salaries')
+          expect(salaries[:data][:attributes]).to be_a(Hash)
+          expect(salaries[:data][:attributes][:destination]).to be_a(String)
+          expect(salaries[:data][:attributes][:forecast]).to be_a(Hash)
+          expect(salaries[:data][:attributes][:forecast][:summary]).to be_a(String)
+          expect(salaries[:data][:attributes][:forecast][:temperature]).to be_a(Float)
+          expect(salaries[:data][:attributes][:salaries]).to be_an(Array)
+
+          salaries[:data][:attributes][:salaries].each do |salary|
+            expect(salary).to be_a(Hash)
+            expect(salary[:title]).to be_a(String)
+            expect(salary[:min]).to be_a(Float)
+            expect(salary[:max]).to be_a(Float)
+          end
         end
       end
     end
