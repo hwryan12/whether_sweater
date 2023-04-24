1841736c46c1c349f97dfcfaa0a093a5e47ffb9a
Harrison Ryan <hwryan12@gmail.com>
2023-04-24 11:49:13 -0500

    refactor: Update test to account for PORO being used

--- spec/final/salaries_facade_spec.rb
+++ spec/final/salaries_facade_spec.rb
@@ -7,23 +7,17 @@ RSpec.describe SalariesFacade, type: :facade do
       it 'returns a tech salaries for a given city as serialzed JSON' do
         VCR.use_cassette('salaries_facade') do
           salaries = SalariesFacade.new.get_tech_salaries(city)
-          binding.pry
-          expect(salaries).to be_a(Hash)
-          expect(salaries[:data]).to be_a(Hash)
-          expect(salaries[:data][:id]).to be_nil
-          expect(salaries[:data][:type]).to eq('salaries')
-          expect(salaries[:data][:attributes]).to be_a(Hash)
-          expect(salaries[:data][:attributes][:destination]).to be_a(String)
-          expect(salaries[:data][:attributes][:forecast]).to be_a(Hash)
-          expect(salaries[:data][:attributes][:forecast][:summary]).to be_a(String)
-          expect(salaries[:data][:attributes][:forecast][:temperature]).to be_a(Float)
-          expect(salaries[:data][:attributes][:salaries]).to be_an(Array)
-
-          salaries[:data][:attributes][:salaries].each do |salary|
-            expect(salary).to be_a(Hash)
-            expect(salary[:title]).to be_a(String)
-            expect(salary[:min]).to be_a(Float)
-            expect(salary[:max]).to be_a(Float)
+          # binding.pry
+          expect(salaries).to be_a(Salary)
+          expect(salaries.destination).to be_a(String)
+          expect(salaries.summary).to be_a(String)
+          expect(salaries.temperature).to be_a(String)
+          expect(salaries.jobs).to be_an(Array)
+          
+          salaries.jobs.each do |job|
+            expect(job[:title]).to be_a(String)
+            expect(job[:min]).to be_a(String)
+            expect(job[:max]).to be_a(String)
           end
         end
       end
